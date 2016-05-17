c3e1ebb70734c3527a919a343e9a04fac399c2c5
Samuel Toloza <tlz.system@gmail.com>
2016-05-17 12:42:49 -0400

    valida rut

---
+++ validarut.py
@@ -0,0 +1,28 @@
+import sys
+from itertools import cycle
+
+def validarRut(rut):
+	rut = rut.upper();
+	rut = rut.replace("-","")
+	rut = rut.replace(".","")
+	aux = rut[:-1]
+	dv = rut[-1:]
+
+	revertido = map(int, reversed(str(aux)))
+	factors = cycle(range(2,8))
+	s = sum(d * f for d, f in zip(revertido,factors))
+	res = (-s)%11
+
+	if str(res) == dv:
+		return True
+	elif dv=="K" and res==10:
+		return True
+	else:
+		return False
+
+
+def main():
+	print(validarRut(sys.argv[1] ))
+
+if __name__=="__main__":
+	main()
